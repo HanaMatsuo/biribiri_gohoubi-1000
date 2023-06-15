21b2a90223b7f477d810fd2ebced99e00bf71cd7
matsuo hana <matsuohana@matsuonoMBP.tnaka1.zaq.ne.jp>
2023-06-16 04:42:21 +0900

    add image in banner

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(function($){
         autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
         speed: 3000, // スライドが流れる速度を設定
         cssEase: "linear", // スライドの流れ方を等速に設定
-        slidesToShow: 5, // 表示するスライドの数
+        slidesToShow: 6, // 表示するスライドの数
         swipe: false, // 操作による切り替えはさせない
         arrows: false, // 矢印非表示
         pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
