6b6d8c57472fed114ee46ba88502d6fed1dd9094
matsuo hana <matsuohana@matsuonoMBP.tnaka1.zaq.ne.jp>
2023-06-16 00:19:59 +0900

    fadein

--- js/main.js
+++ js/main.js
@@ -41,23 +41,26 @@ $(function($){
       
       
       $(window).scroll(function (){
-
         $('.fadein').each(function(){
-    
           var targetElement = $(this).offset().top;
-    
           var scroll = $(window).scrollTop();
-    
           var windowHeight = $(window).height();
-    
           if (scroll > targetElement - windowHeight + 200){
-    
             $(this).css('opacity','1');
-    
             $(this).css('transform','translateY(0)');
-    
           }
+        });
     
+       });
+       $(window).scroll(function (){
+        $('.fadein-footer').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight ){
+            $(this).css('opacity','1');
+            $(this).css('transform','translateY(0)');
+          }
         });
     
        });
